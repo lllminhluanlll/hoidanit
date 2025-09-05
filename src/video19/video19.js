04dda1b86dfff88c68f03134849725766e9470dc
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 02:51:33 +0700

    video19

---
+++ src/video19/video19.js
@@ -0,0 +1,15 @@
+console.log("video 19")
+
+//object
+
+const person = {
+    name: "hml",
+    age: 23,
+    style: "nope"
+}
+console.log(person)
+
+//array
+
+const score = [0, 5, 8, 10, 6]
+console.log(score)
