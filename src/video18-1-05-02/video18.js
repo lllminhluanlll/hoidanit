0c2894bb3055a87bc4cdce21e03ccb0e690003b1
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 02:38:42 +0700

    video18

---
+++ src/video18-1-05-02/video18.js
@@ -0,0 +1,26 @@
+//number
+const myAge = 23;
+const myCore = 5;
+
+console.log("video18")
+
+
+
+//string
+
+const name = "hml";
+const name1 = "dcl";
+const name2 = "lmht";
+console.log(name)
+
+//boolean: true/false
+const dabanhsieuhay = true;
+console.log(dabanhsieuhay)
+
+//undefined (chưa gán giá trị =)
+let hml;
+console.log(hml)
+
+//null
+let testNull = null;
+console.log(testNull)
