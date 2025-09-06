daa70e7dd167f0ba4e4eac5bdc25019f4c6212af
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 21:26:10 +0700

    video29

---
+++ src/chapter04/video29/video29.js
@@ -0,0 +1,19 @@
+console.log("video 29")
+
+//break
+// for (let i = 0; i < 10; i++) {
+//     console.log("i =", i)
+//     if (i === 7) {
+//         break;
+//     }
+// }
+
+
+//coutinue
+for (let i = 0; i < 10; i++) {
+
+    if (i === 7) {
+        continue;
+    }
+    console.log("i =", i)
+}
