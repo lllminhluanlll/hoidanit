5e804bee54d35622fef210f50a35d053e8312ede
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 22:01:38 +0700

    video31

---
+++ src/chapter04/video31/video31.js
@@ -0,0 +1,11 @@
+// console.log("video 31")
+// function sum(a, b) {
+//     return a + b;
+// }
+// console.log(sum(5, 10))
+
+const sum = (a, b) => {
+    return a + b;
+
+}
+console.log(sum(30, 10));
