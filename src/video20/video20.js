2bbc35354aabb20496460f06861b97bbef3c4104
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 18:29:15 +0700

    video20

---
+++ src/video20/video20.js
@@ -0,0 +1,16 @@
+console.log("video 20")
+
+
+// let a = 4
+
+// // const b = ++a =>> ++a la tang ngay lap tuc gia tri
+// // const b = a++ =>> a++ la chay ra gia tri roi moi tang
+// const c = a--
+
+// console.log("a = " + a)
+// console.log("c = " + c)
+// // console.log("b = " + b)
+
+const a = 5;
+const b = "5";
+console.log(a === b)
