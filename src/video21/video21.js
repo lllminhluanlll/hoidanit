94c6d79ba76bdab96010dbaa65c5c4af81c1e62c
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 18:57:13 +0700

    video21

---
+++ src/video21/video21.js
@@ -0,0 +1,13 @@
+console.log("video21")
+
+console.log("info")
+console.warn("test warning")
+console.error("oops my mistake")
+
+let name = "hoidanit";
+let age = 20;
+// Không nên: cộng chuỗi dễ sai
+console.log("Name: " + name + ", Age: " + age);
+console.log("Name :", name, ", Age: ", age)
+
+console.log("%cCảnh báo!", "color: red; font-weight: bold;");
