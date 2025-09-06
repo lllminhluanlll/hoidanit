2c42fcd1dc2e09ec7eb66621911f4cbfbaec1f18
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 21:10:48 +0700

    video28

---
+++ src/chapter04/video28/video28.js
@@ -0,0 +1,18 @@
+console.log("video 28")
+//for
+for (let i = 0; i < 5; i++) {
+    console.log("i = ", i)
+}
+
+//while
+let score = 10
+while (score > 5) {
+    console.log("check score: ", score);
+    score--;
+}
+// do while
+let score1 = 10
+do {
+    console.log("check score1: ", score1);
+    score1--;
+} while (score1 > 50);
