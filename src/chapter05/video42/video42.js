3b617d15056e8a6b2e79ef4aee45727863ae267a
Hmlne <huynhminhluan1508@gmail.com>
2025-09-07 05:32:00 +0700

    video42

---
+++ src/chapter05/video42/video42.js
@@ -0,0 +1,18 @@
+console.log("video 42")
+
+const sroce = [10, 5, 6, 7, 8];
+
+const sv1 = {
+    name: "hml",
+    score: 6,
+    address: "Đồng Tháp"
+}
+
+const sv2 = {
+    name: "Minh Ly",
+    score: 4,
+    address: "Lai Vung"
+}
+const sinhvien = [sv1, sv2]
+console.log("score:", sroce);
+console.log("sinhvien:", sinhvien);
