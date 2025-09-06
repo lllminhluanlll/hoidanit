ea2bdaa33b856dc0c02598b0a41aa3753ac5af39
Hmlne <huynhminhluan1508@gmail.com>
2025-09-06 19:48:57 +0700

    video26

---
+++ src/chapter04/video26/video26.js
@@ -0,0 +1,22 @@
+console.log("video 26")
+
+// if / if-else // else-if
+const age = 10;
+
+// if - else
+if (age > 18) {
+    console.log("enjoy your video")
+} else {
+    console.log("you can't watch this video")
+}
+
+const score = 10;
+if (score > 9) {
+    console.log("Gioi")
+}
+else if (score > 6 && score < 9) {
+    console.log("Kha")
+}
+else {
+    console.log("Trung binh")
+}
