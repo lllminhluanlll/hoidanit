eba363986a8e045105ee0646b83a16060d2fe9a5
Hmlne <huynhminhluan1508@gmail.com>
2025-09-07 18:25:02 +0700

    video 50-51

---
+++ src/chapter06/video50/video50.js
@@ -0,0 +1,9 @@
+console.log("video50")
+
+const Btn = document.getElementById("myBtn");
+
+console.log(Btn)
+
+function clickBtn() {
+    console.log("Hãy click tôi")
+}
---
+++ src/chapter06/video51/video51.js
@@ -0,0 +1,9 @@
+console.log("video51")
+
+const btn = document.getElementById("Btnhml");
+
+btn.addEventListener("click", function () {
+    console.log("hãy click vào tôi ")
+})
+
+console.log(btn)
