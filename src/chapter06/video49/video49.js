e61bfc8a3e9a2a7d58f50d7ff1fd948e3b63ac72
Hmlne <huynhminhluan1508@gmail.com>
2025-09-07 17:58:53 +0700

    video 49

---
+++ src/chapter06/video49/video49.js
@@ -0,0 +1,6 @@
+console.log("video 49")
+
+const element = document.getElementById("username"); //dùng để lấy id
+const myClass = document.querySelector(".hml"); //dùng lấy class cần thêm .
+const allmyClass = document.querySelectorAll(".hml");//dùng lấy tất cả có cùng class cũng cần thêm .
+console.log(allmyClass)
